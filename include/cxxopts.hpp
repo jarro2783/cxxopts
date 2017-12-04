6099398a3cca0d2e1018cf342a76a046b79f8165
Masashi Fujita <objectxtreme@gmail.com>
2017-12-04 09:04:00 +0900

    fix: Fix `false` pattern too
    cf. 318055b811e1578027431a31ecaadd7c8fb6177f

--- include/cxxopts.hpp
+++ include/cxxopts.hpp
@@ -446,7 +446,7 @@ namespace cxxopts
       std::basic_regex<char> truthy_pattern
         ("(t|T)(rue)?");
       std::basic_regex<char> falsy_pattern
-        ("(f|false|F|False)?");
+        ("((f|F)(alse)?)?");
     }
 
     namespace detail
@@ -1570,7 +1570,7 @@ ParseResult::consume_positional(std::string a)
     if (iter != m_options.end())
     {
       auto& result = m_results[iter->second];
-      if (!iter->second->value().is_container()) 
+      if (!iter->second->value().is_container())
       {
         if (result.count() == 0)
         {
