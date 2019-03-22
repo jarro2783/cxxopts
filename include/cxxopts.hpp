8c78f0e3af2056521d491c3a643770440689ad2a
Linus Sherrill <linus.sherrill@kitware.com>
2019-03-22 14:47:58 -0400

    Remove check for container when generating help message.
    Some positional parameters would be listed in the help text and others
    would not, when what is desired is that no positional parameters are
    listed with the other command options. This change suppresses the help
    listing for all positional parameters.

--- include/cxxopts.hpp
+++ include/cxxopts.hpp
@@ -1938,8 +1938,7 @@ Options::help_one_group(const std::string& g) const
 
   for (const auto& o : group->second.options)
   {
-    if (o.is_container &&
-        m_positional_set.find(o.l) != m_positional_set.end() &&
+    if (m_positional_set.find(o.l) != m_positional_set.end() &&
         !m_show_positional)
     {
       continue;
@@ -1958,8 +1957,7 @@ Options::help_one_group(const std::string& g) const
   auto fiter = format.begin();
   for (const auto& o : group->second.options)
   {
-    if (o.is_container &&
-        m_positional_set.find(o.l) != m_positional_set.end() &&
+    if (m_positional_set.find(o.l) != m_positional_set.end() &&
         !m_show_positional)
     {
       continue;
