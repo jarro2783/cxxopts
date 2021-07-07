e453d2c79de09784154bce0db674f7af744e9d5b
zrshonor <2058084624@qqc.com>
2021-07-07 15:05:02 +0800

    change

--- src/example.cpp
+++ src/example.cpp
@@ -1,171 +1,57 @@
-/*
 
-Copyright (c) 2014 Jarryd Beck
+#include<cxxopts.hpp>
+#include<iostream>
+#include<vector>
+using namespace std;
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+void parse(int argc,const char* argv[]){
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+    try{
+        cxxopts::Options options(argv[0],"My test example");
+        options.allow_unrecognised_options();
+        options.show_positional_help();
+        options.set_width(70).set_tab_expansion().add_options("MyOption")
+                ("f,float","a float variable",cxxopts::value<float>()->implicit_value("10"),"file")
+                ("i,int","a integer variable",cxxopts::value<int>())
+                ("l,float_list","a float list",cxxopts::value<vector<float>>())
+                ("help","print help");
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+        auto result = options.parse(argc,argv);
 
-*/
-
-#include <iostream>
-
-#include "cxxopts.hpp"
-
-void
-parse(int argc, const char* argv[])
-{
-  try
-  {
-    cxxopts::Options options(argv[0], " - example command line options");
-    options
-      .positional_help("[optional args]")
-      .show_positional_help();
-
-    bool apple = false;
-
-    options
-      .set_width(70)
-      .set_tab_expansion()
-      .allow_unrecognised_options()
-      .add_options()
-      ("a,apple", "an apple", cxxopts::value<bool>(apple))
-      ("b,bob", "Bob")
-      ("char", "A character", cxxopts::value<char>())
-      ("t,true", "True", cxxopts::value<bool>()->default_value("true"))
-      ("f, file", "File", cxxopts::value<std::vector<std::string>>(), "FILE")
-      ("i,input", "Input", cxxopts::value<std::string>())
-      ("o,output", "Output file", cxxopts::value<std::string>()
-          ->default_value("a.out")->implicit_value("b.def"), "BIN")
-      ("x", "A short-only option", cxxopts::value<std::string>())
-      ("positional",
-        "Positional arguments: these are the arguments that are entered "
-        "without an option", cxxopts::value<std::vector<std::string>>())
-      ("long-description",
-        "thisisareallylongwordthattakesupthewholelineandcannotbebrokenataspace")
-      ("help", "Print help")
-      ("tab-expansion", "Tab\texpansion")
-      ("int", "An integer", cxxopts::value<int>(), "N")
-      ("float", "A floating point number", cxxopts::value<float>())
-      ("vector", "A list of doubles", cxxopts::value<std::vector<double>>())
-      ("option_that_is_too_long_for_the_help", "A very long option")
-    #ifdef CXXOPTS_USE_UNICODE
-      ("unicode", u8"A help option with non-ascii: à. Here the size of the"
-        " string should be correct")
-    #endif
-    ;
-
-    options.add_options("Group")
-      ("c,compile", "compile")
-      ("d,drop", "drop", cxxopts::value<std::vector<std::string>>());
-
-    options.parse_positional({"input", "output", "positional"});
-
-    auto result = options.parse(argc, argv);
-
-    if (result.count("help"))
-    {
-      std::cout << options.help({"", "Group"}) << std::endl;
-      exit(0);
+        if(result.count("help")){
+            cout<<options.help({"MyOption"})<<endl;
+            exit(0);
+        }
+        if(result.count("i")){
+            cout<<"Saw the argument of int "<<result.count("i")<<" times"<<endl;
+            cout<<"The last value is "<<result["i"].as<int>()<<endl;
+        }
+        if(result.count("f")){
+            cout<<"Saw the argument of float "<<result.count("f")<<" times"<<endl;
+            cout<<"The last value is "<<result["f"].as<float>()<<endl;
+        }
+        if(result.count("l")){
+            auto list = result["l"].as<vector<float>>();
+            for(auto data:list){
+                cout<<data<<" ";
+            }
+            cout<<endl;
+        }
+        auto unmatched = result.unmatched();
+        for(auto data:unmatched){
+            cout<<data<<" ";
+        }
+        cout<<endl;
+    }catch (cxxopts::OptionException &e){
+        e.what();
     }
-
-    if (apple)
-    {
-      std::cout << "Saw option ‘a’ " << result.count("a") << " times " <<
-        std::endl;
-    }
-
-    if (result.count("b"))
-    {
-      std::cout << "Saw option ‘b’" << std::endl;
-    }
-
-    if (result.count("char"))
-    {
-      std::cout << "Saw a character ‘" << result["char"].as<char>() << "’" << std::endl;
-    }
-
-    if (result.count("f"))
-    {
-      auto& ff = result["f"].as<std::vector<std::string>>();
-      std::cout << "Files" << std::endl;
-      for (const auto& f : ff)
-      {
-        std::cout << f << std::endl;
-      }
-    }
-
-    if (result.count("input"))
-    {
-      std::cout << "Input = " << result["input"].as<std::string>()
-        << std::endl;
-    }
-
-    if (result.count("output"))
-    {
-      std::cout << "Output = " << result["output"].as<std::string>()
-        << std::endl;
-    }
-
-    if (result.count("positional"))
-    {
-      std::cout << "Positional = {";
-      auto& v = result["positional"].as<std::vector<std::string>>();
-      for (const auto& s : v) {
-        std::cout << s << ", ";
-      }
-      std::cout << "}" << std::endl;
-    }
-
-    if (result.count("int"))
-    {
-      std::cout << "int = " << result["int"].as<int>() << std::endl;
-    }
-
-    if (result.count("float"))
-    {
-      std::cout << "float = " << result["float"].as<float>() << std::endl;
-    }
-
-    if (result.count("vector"))
-    {
-      std::cout << "vector = ";
-      const auto values = result["vector"].as<std::vector<double>>();
-      for (const auto& v : values) {
-        std::cout << v << ", ";
-      }
-      std::cout << std::endl;
-    }
-
-    std::cout << "Arguments remain = " << argc << std::endl;
-
-    auto arguments = result.arguments();
-    std::cout << "Saw " << arguments.size() << " arguments" << std::endl;
-  }
-  catch (const cxxopts::OptionException& e)
-  {
-    std::cout << "error parsing options: " << e.what() << std::endl;
-    exit(1);
-  }
 }
 
-int main(int argc, const char* argv[])
-{
-  parse(argc, argv);
-
-  return 0;
+int main(){
+    int argc = 11;
+    const char* argv[] = {"My_Group","-int","10","-i=30","20","-f","30","-help","40","-l","30,20,30,40,50,60"};
+    parse(argc,argv);
+    return 0;
 }
+
+
